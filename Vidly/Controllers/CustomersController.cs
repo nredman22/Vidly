5c7e227a6834469033a98945e63eeac648d1fe12
nredman <nredm@DESKTOP-VQ5GQ2U>
2019-08-16 00:50:48 -0500

    added ability to update

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -25,24 +25,50 @@ namespace Vidly.Controllers
 
         public ActionResult New()
         {
-            var membershipTypes = _context.MembershipTypes.ToList();
-
-            var viewModel = new NewCustomerViewModel
+            var viewModel = new CustomerFormViewModel
             {
-                MembershipTypes = membershipTypes,
+                MembershipTypes = _context.MembershipTypes.ToList(),
                 Customer = new Customer()
             };
 
-            return View(viewModel);
+            return View("CustomerForm", viewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.ID == 0)   // new customer
+                _context.Customers.Add(customer);
+            else     // existing customer
+            {
+                var existingCustomer = _context.Customers.Single(c => c.ID == customer.ID);
+
+                // manually change only the fields you want changed -- could use library automapper to update fields
+                existingCustomer.Name = customer.Name;
+                existingCustomer.Birthdate = customer.Birthdate;
+                existingCustomer.MembershipTypeId = customer.MembershipTypeId;
+                existingCustomer.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
             _context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var foundCustomer = _context.Customers.SingleOrDefault(customer => customer.ID == id);
+
+            if (foundCustomer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = _context.MembershipTypes.ToList(),
+                Customer = foundCustomer
+            };
+
+            return View("CustomerForm", viewModel);
         }
 
         public ActionResult Index()
